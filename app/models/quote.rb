cfb518aaca6b17b642540e67b162d00e4037150b
BillW <billwagner102@gmail.com>
2019-09-05 12:48:50 -0700

    Validations and error messages for quote parameters.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 240, minimum: 5 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 4 }
 end
